2eed07aa1b05f8088e4ab6f1e2fbad3117065b16
quipsquaff <quipsquaff@gmail.com>
2018-11-29 20:44:45 -0800

    small update

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,7 @@
-// Displays all characters
 app.get("/api/reserve", function (req, res) {
     return res.json(tables);
 });
 
-// Displays a single character, or returns false
 app.get("/api/tables", function (req, res) {
     var chosen = req.params.tables;
 });
